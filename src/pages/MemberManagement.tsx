c4a6a065e98f9cfe70b476323aa135349bdea055
timilehin_princeps <timilehin.oladapo@princepsfinance.com>
2025-05-25 23:43:38 +0100

    minor changes

--- src/pages/MemberManagement.tsx
+++ src/pages/MemberManagement.tsx
@@ -1,52 +1,9 @@
-import React from "react";
 import Layout from "../components/layout/Layout";
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuItem,
-} from "@radix-ui/react-dropdown-menu";
-import { ColumnDef } from "@tanstack/react-table";
-import { CiMenuKebab } from "react-icons/ci";
-import { GrNotes } from "react-icons/gr";
-import { IoCallOutline } from "react-icons/io5";
-import { MdAddCircleOutline } from "react-icons/md";
-import { Member } from "../interfaces/Member";
+// import { Member } from "../interfaces/Member";
 
 function MemberManagement() {
   // follow up pill
-  const handleViewMoreModal = (handleViewMoreModal: Member) => {};
-  const columns: ColumnDef<Member>[] = [
-    {
-      accessorKey: "full_name",
-      header: "Name",
-    },
-    {
-      accessorKey: "phone",
-      header: "Phone",
-    },
-    {
-      accessorKey: "created_at",
-      header: () => <span className="hidden md:table-cell">Date Joined</span>,
-      cell: ({ row }) => {
-        const date = new Date(row.original.created_at!);
-        return (
-          <div className="hidden md:block">{date.toLocaleDateString()}</div>
-        );
-      },
-    },
-
-    {
-      id: "actions",
-      header: "Actions",
-      cell: ({ row }) => {
-        const member = row.original;
-        return (
-          <button onClick={() => handleViewMoreModal(member)}>view</button>
-        );
-      },
-    },
-  ];
+  // const handleViewMoreModal = (_handleViewMoreModal: Member) => {};
   return (
     <Layout>
       {" "}
