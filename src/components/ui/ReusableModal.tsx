8a8d12a6efd22435e4bf625462f2f2cbb44460fb
timilehin_princeps <timilehin.oladapo@princepsfinance.com>
2025-08-10 00:23:52 +0100

    right margin to modal title

--- src/components/ui/ReusableModal.tsx
+++ src/components/ui/ReusableModal.tsx
@@ -1,6 +1,10 @@
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "./dialog";
-
-
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "./dialog";
 
 interface ReusableModalProps {
   open: boolean;
@@ -22,7 +26,7 @@ export default function ReusableModal({
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="w-[85%] sm:w-[85%] my-8  max-w-md md:max-w-lg lg:max-w-xl mx-auto bg-white text-[#44444B] dark:bg-[#242529] dark:text-white">
         <DialogHeader className="text-left mb-4">
-          <DialogTitle className="mb-2 capitalize">{title}</DialogTitle>
+          <DialogTitle className="mb-2 capitalize mr-1">{title}</DialogTitle>
           {description && (
             <DialogDescription className="mb-4">
               {description}
